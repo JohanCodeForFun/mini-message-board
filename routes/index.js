7c1351c83c36643b082762eff6668e71833f7e14
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-10-04 20:28:40 +0200

    add array of messages and show on page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amanda",
+    added: new Date()
+  },
+  {
+    text: "Hey, how are you?",
+    user: "Becky",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Mini Message Board',
+    messages: messages
+   },
+  );
 });
 
 module.exports = router;
